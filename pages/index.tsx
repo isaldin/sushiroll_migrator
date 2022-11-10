2ea473c0014c2146cc0ce507b26ec199c49432ba
saldin <saldin.iliya@gmail.com>
2022-11-10 21:30:26 +0600

    Add Web3React.

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,28 +2,41 @@ import React from "react";
 import Tabs from "../components/ common/Tabs";
 import TokensList from "../components/TokensList";
 import LPPositions from "../components/LPPositions";
+import { useWeb3React } from "@web3-react/core";
+import { InjectedConnector } from "@web3-react/injected-connector";
 
 type MainPageProps = {
   //
 };
 
-const MainPage: React.FC<MainPageProps> = () => (
-  <div>
-    <Tabs
-      tabs={[
-        {
-          id: "tokens",
-          name: "Tokens List",
-          component: <TokensList />,
-        },
-        {
-          id: "lp",
-          name: "LP Positions",
-          component: <LPPositions />,
-        },
-      ]}
-    ></Tabs>
-  </div>
-);
+const MainPage: React.FC<MainPageProps> = () => {
+  const { active, activate } = useWeb3React();
+
+  if (!active) {
+    const injectedConnector = new InjectedConnector({
+      supportedChainIds: [1, 5],
+    });
+    activate(injectedConnector);
+  }
+
+  return (
+    <div>
+      <Tabs
+        tabs={[
+          {
+            id: "tokens",
+            name: "Tokens List",
+            component: <TokensList />,
+          },
+          {
+            id: "lp",
+            name: "LP Positions",
+            component: <LPPositions />,
+          },
+        ]}
+      ></Tabs>
+    </div>
+  );
+};
 
 export default MainPage;
